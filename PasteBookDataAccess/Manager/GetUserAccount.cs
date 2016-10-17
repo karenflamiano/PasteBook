124568119f202480ac1635064b9e5f230953a280
Bootcamp <Bootcamp@RAINFALL>
2016-10-17 21:39:09 +0800

    Another login function

---
+++ PasteBookDataAccess/Manager/GetUserAccount.cs
@@ -0,0 +1,33 @@
+﻿using PasteBookDataAccess.Entities;
+using PasteBookEntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PasteBookDataAccess.Manager
+{
+    public class GetUserAccount
+    {
+        List<Exception> ListOfException = new List<Exception>();
+        public bool GetAccount(string emailAddress)
+        {
+            bool result = false;
+            try
+            {
+                using (var context = new PASTEBOOKEntities())
+                {
+                    result = context.USERs.Any(x => x.EMAIL_ADDRESS == emailAddress);
+                }
+            }
+            catch (Exception ex)
+            {
+                ListOfException.Add(ex);
+            }
+
+            return result;
+
+        }
+    }
+}
